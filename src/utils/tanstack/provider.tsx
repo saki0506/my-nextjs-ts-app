54240076e1dcb47d7e7a97ff66ce89f97e84bfef
saki0506 <172128609+saki0506@users.noreply.github.com>
2025-08-22 07:00:49 +0900

    "feat: Add React Query DevTools for debugging"

--- src/utils/tanstack/provider.tsx
+++ src/utils/tanstack/provider.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface TanstackProviderProps {
   children: React.ReactNode;
 }
+
 const TanstackQueryProvider: React.FC<TanstackProviderProps> = ({
   children,
 }) => {
   const queryClient = new QueryClient();
+
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
